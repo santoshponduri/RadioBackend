0674b47c5d4d80a82c5696fb456ce118a37f07cd
santoshponduri <santoshponduri@gmail.com>
2023-10-29 18:16:40 +0530

    test

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,18 +1,25 @@
-const mysql = require('mysql');
+const mysql = require("mysql2");
 
 const dbConfig = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "RadioZindagi"
-})
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "RadioZindagi",
+});
 
-dbConfig.connect((err) => { 
-    if (err) { 
-      console.log("Database Connection Failed !!!", err); 
-    } else { 
-      console.log("connected to Database"); 
-    } 
-}); 
+// const dbConfig = mysql.createConnection({
+//   host: "sql.freedb.tech",
+//   user: "freedb_radiouser",
+//   password: "js!mNKH9xSAsgE@",
+//   database: "freedb_radiozindagi",
+// });
+
+dbConfig.connect((err) => {
+  if (err) {
+    console.log("Database Connection Failed !!!", err);
+  } else {
+    console.log("connected to Database");
+  }
+});
 
 module.exports = dbConfig;
